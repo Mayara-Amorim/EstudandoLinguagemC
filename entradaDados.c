9cfba836f0ec3339e9c9180d955100d8de79a3f5
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-08 18:11:40 -0300

    Estrutura condicional encadeada

--- entradaDados.c
+++ entradaDados.c
@@ -2,9 +2,27 @@
 #include <locale.h>
 
 int main(){
+    setlocale(LC_ALL, "");
     int idade;
+    char eCivil;
     printf("Digite sua idade: ");
     scanf("%d", &idade);
     printf("%d anos", idade);
+    printf("\nDigite C para casado ou S para solteiro: ");
+    fflush(stdin);
+    eCivil = getchar();
+    printf("\nVc digitou %c", eCivil);
+
+    if (idade >= 18){
+            printf("\nVocê é maior");
+    }else{
+        if(idade <=12){
+            printf("\nIh, vc é uma criança!");
+        }
+       else{
+        printf("\nVocê é menor!");
+       }
+    }
+
 
 }
